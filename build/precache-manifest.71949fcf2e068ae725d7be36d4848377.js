06cf7863d375dab0de342707c2e3f2f194f0a5d1
Andrii Tynok <andriytinok@gmail.com>
2019-08-07 17:25:17 +0300

    uibuild

---
+++ build/precache-manifest.71949fcf2e068ae725d7be36d4848377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2d0d3605b8ad662c29014f5d1f62cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a20c6af41e296cc83fd6",
+    "url": "/static/css/main.a41b7b1b.chunk.css"
+  },
+  {
+    "revision": "da776ff6faf2792a84a5",
+    "url": "/static/js/2.47e200b3.chunk.js"
+  },
+  {
+    "revision": "a20c6af41e296cc83fd6",
+    "url": "/static/js/main.e2024767.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
